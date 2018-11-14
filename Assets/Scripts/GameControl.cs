2c062406a5d06bf3434f917399d0fc132973e457
Design <jaekyun.kang@infosys.com>
2018-11-13 18:02:04 -0800

    first commit

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+	Dictionary<string, string> textList = new Dictionary<string, string> ();
+
+	private bool detected = false;
+	private bool flag = false;
+
+	public GameObject text1;
+	public GameObject confirmButton;
+	public GameObject resetButton;
+
+	public Animator animHDMI;
+	public Animator animPower;
+
+	public GameObject boxHDMI;
+	public GameObject boxPower;
+
+	private string text;
+	private int sequence = -1;
+
+	/*
+	public enum TextManager
+	{
+		requireScan = "Please scan the Osprey", 
+		scanComplete = "Scan Complete!",
+		plugHDMI = "Please plug in the HDMI cable",
+		completeHDMI = "HDMI connected!",
+		plugPOWER = "Please plug in the Power cable",
+		completePower = "Power connected!",
+		setupDone = "Set up Completed!"
+	};
+	*/
+
+	void Start(){
+		sequence = 0;
+
+		SetDictionary ();
+		resetButton.SetActive (false);
+		confirmButton.SetActive (false);
+	}
+
+
+	void SetDictionary(){
+		textList.Add ("requireScan", "Please scan the Osprey");
+		textList.Add ("scanComplete", "Scan Complete!");
+		textList.Add ("plugHDMI", "Please plug in the HDMI cable");
+		textList.Add ("completeHDMI", "HDMI connected!");
+		textList.Add ("plugPOWER", "Please plug in the Power cable");
+		textList.Add ("completePower", "Power connected!");
+		textList.Add ("setupDone", "Set up Complete!");
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (sequence == -1) {
+				Debug.Log (textList["requireScan"]);
+			} else if (sequence == 0) {
+				Debug.Log (textList["scanComplete"]);
+			} else if (sequence == 1) {
+				Debug.Log (textList["plugHDMI"]);
+			} else if (sequence == 2) {
+				Debug.Log (textList["completeHDMI"]);
+			} else if (sequence == 3) {
+				Debug.Log (textList["plugPOWER"]);
+			} else if (sequence == 4) {
+				Debug.Log (textList["completePower"]);
+			} else if (sequence == 5) {
+				Debug.Log (textList["setupDone"]);
+			}
+			sequence++;
+
+		}
+
+	}
+
+	public void OnConfirmButtonClicked(){
+
+	}
+
+	public void OnResetButtonClicked(){
+
+	}
+
+	IEnumerator DelayFunction(int sec){
+		yield return new WaitForSeconds (sec);
+	}
+	/*
+	// Use this for initialization
+	void Start () {
+		sequence = 0;
+		resetButton.SetActive (false);
+		confirmButton.SetActive (false);
+		boxHDMI.SetActive (false);
+		boxPower.SetActive (false);
+		text = "Please scan the object";
+		text1.GetComponent<Text> ().text = text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Space)) {
+			sequence = 1;
+			flag = true;
+		}
+		detected = GameObject.Find ("GenerateImageAnchorCube").GetComponent<GenerateImageAnchor> ().isObjectDetected;
+		if (detected && !flag) {
+			sequence = 1;
+			flag = true;
+		}
+		if (sequence > 0) {
+			TextControl (sequence);
+		}
+		if (sequence == 0) {
+			resetButton.SetActive (false);
+		}
+		Debug.Log (sequence);
+	}
+
+
+	public void OnConfirmButtonClicked(){
+		if (sequence == 1) {
+			sequence = 2;
+			animHDMI.Play ("hdmi_animation");
+		} else if (sequence == 2) {
+			sequence = 3;
+			animPower.Play ("PowerJack_animation");
+		}
+	}
+
+	public void OnResetButtonClicked(){
+			
+		confirmButton.SetActive (false);
+		text = "Please scan the object";
+		text1.GetComponent<Text> ().text = text;
+		sequence = 0;
+		animHDMI.Play ("hdmiIdle");
+		animPower.Play ("poweridle");
+		boxHDMI.SetActive (false);
+		boxPower.SetActive (false);
+		flag = false;
+	}
+
+	public void TextControl(int num){
+		switch (num) {
+		case 1:	
+			confirmButton.SetActive (true);
+			text = "Please plug in the hdmi cable";
+			text1.GetComponent<Text> ().text = text;
+			boxHDMI.SetActive (true);
+			break;
+		case 2:
+			text = "Please plugin the power cable";
+			text1.GetComponent<Text> ().text = text;
+			StartCoroutine ("TimerCount");
+
+			break;
+
+		case 3:
+			text = "Set up done!";
+			text1.GetComponent<Text> ().text = text;
+			resetButton.SetActive (true);
+			break;
+		case 0:
+			text = "Please scan the object";
+			text1.GetComponent<Text> ().text = text;
+			resetButton.SetActive (false);
+			boxHDMI.SetActive (false);
+			boxPower.SetActive (false);
+			flag = false;
+			break;
+		}
+	}
+
+	IEnumerator TimerCount(){
+		yield return new WaitForSeconds (2.0f);
+		boxPower.SetActive (true);
+	}
+
+	*/
+
+}
---
+++ Assets/Scripts/MovingToBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingToBox : MonoBehaviour {
+
+	public bool ready;
+	public Transform target;
+
+	private Transform orininPos;
+	// Use this for initialization
+	void Start () {
+		ready = false;
+		orininPos = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ready == true) {
+			Debug.Log ("please move");
+			float step =.05f * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		}
+	}
+}
